b6826ba0ad31adb7cf561b0abc545a63283842bd
sebastian.sellmair <sebastian.sellmair@jetbrains.com>
2021-01-14 18:35:34 +0100

    fixup! Remove KotlinMetadataTarget's KotlinTargetComponent in favour of KotlinSoftwareComponent.kt

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/HierarchicalMppIT.kt
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/HierarchicalMppIT.kt
@@ -26,7 +26,7 @@ class HierarchicalMppIT : BaseGradleIT() {
         get() = gradleVersion
 
     companion object {
-        private val gradleVersion = GradleVersionRequired.Exact("6.5")
+        private val gradleVersion = GradleVersionRequired.FOR_MPP_SUPPORT
     }
 
     @Test
